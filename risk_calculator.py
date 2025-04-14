4e3d54a6b76b0828c6e9f29d4a4b31189d005cc5
MGuerra07 <marcelosguerra07@gmail.com>
2025-04-14 14:58:05 -0300

    risk_calculator.py

---
+++ risk_calculator.py
@@ -0,0 +1,7 @@
+def calcular_risco(parametros):
+    """
+    Calcula o risco com base nos parâmetros fornecidos.
+    Substitua os cálculos com as fórmulas apropriadas.
+    """
+    risco = parametros['probabilidade'] * parametros['impacto']  # Exemplo de cálculo simples
+    return risco
